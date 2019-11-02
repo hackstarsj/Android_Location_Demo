58842752ad2e7f738bd76406e3b372a602f06449
sanjeev kumar <sksanjeev362@gmail.com>
2019-11-02 17:33:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidlocationdemo/MainActivity.java
@@ -0,0 +1,211 @@
+package com.example.androidlocationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.app.Service;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static final int PERMISSION_CODE = 101;
+    TextView locationText;
+    Button getLocation;
+    String[] permissions_all={Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION};
+    LocationManager locationManager;
+    boolean isGpsLocation;
+    Location loc;
+    boolean isNetworklocation;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Fetching location...");
+
+        locationText=findViewById(R.id.location);
+        getLocation=findViewById(R.id.getlocation);
+
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                getLocation();
+            }
+        });
+    }
+
+    private void getLocation() {
+        if(Build.VERSION.SDK_INT>=23){
+            if(checkPermission()){
+                getDeviceLocation();
+            }
+            else{
+                requestPermission();
+            }
+        }
+        else{
+            getDeviceLocation();
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,permissions_all,PERMISSION_CODE);
+    }
+
+    private boolean checkPermission() {
+        for(int i=0;i<permissions_all.length;i++){
+            int result= ContextCompat.checkSelfPermission(MainActivity.this,permissions_all[i]);
+            if(result== PackageManager.PERMISSION_GRANTED){
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getDeviceLocation() {
+        //now all permission part complete now let's fetch location
+        locationManager=(LocationManager)getSystemService(Service.LOCATION_SERVICE);
+        isGpsLocation=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworklocation=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if(!isGpsLocation && !isNetworklocation){
+            showSettingForLocation();
+            getLastlocation();
+        }
+        else{
+            getFinalLocation();
+        }
+    }
+
+    private void getLastlocation() {
+        if(locationManager!=null) {
+            try {
+                Criteria criteria = new Criteria();
+                String provider = locationManager.getBestProvider(criteria,false);
+                Location location=locationManager.getLastKnownLocation(provider);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    getFinalLocation();
+                }
+                else{
+                    Toast.makeText(this, "Permission Failed", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void getFinalLocation() {
+        //one thing i missed in permission let's complete it
+        try{
+            if(isGpsLocation){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000*60*1,10,MainActivity.this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if(loc!=null){
+                        updateUi(loc);
+                    }
+                }
+            }
+            else if(isNetworklocation){
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60*1,10,MainActivity.this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(loc!=null){
+                        updateUi(loc);
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this, "Can't Get Location", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (SecurityException e){
+            Toast.makeText(this, "Can't Get Location", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateUi(Location loc) {
+        if(loc.getLatitude()==0 && loc.getLongitude()==0){
+            getDeviceLocation();
+        }
+        else{
+            progressDialog.dismiss();
+            locationText.setText("Location : "+loc.getLatitude()+" , "+loc.getLongitude());
+
+        }
+
+    }
+
+    private void showSettingForLocation() {
+        AlertDialog.Builder al=new AlertDialog.Builder(MainActivity.this);
+        al.setTitle("Location Not Enabled!");
+        al.setMessage("Enable Location ?");
+        al.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+        al.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        al.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateUi(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
